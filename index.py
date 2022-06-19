edbdb0c5b638d1556f34a9bd00c42f7bb578a560
bartoszstepien01 <bartoszstepien01@interia.pl>
2022-06-19 22:21:48 +0200

    Fix for Vercel vol. 2

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-	return 'test'
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
