0aefff6c72ccd71720ee0724390c31ce2c6d2549
bartoszstepien01 <bartoszstepien01@interia.pl>
2022-06-20 21:40:36 +0200

    Messenger bot test

--- index.py
+++ index.py
@@ -1,19 +1,47 @@
-from flask import Flask, Response
+from flask import Flask, request
+
 from substitutions.substitutions import Substitutions
 from substitutions.substitutionsdates import SubstitutionsDates
 from substitutions.substitutionsparser import SubstitutionsParser
 
+from pymessenger.bot import Bot
+
+import os
+
 app = Flask(__name__)
 
-@app.route("/<grade>")
-def catch_all(grade):
-    substitutions = Substitutions()
-    dates = SubstitutionsDates()
-    parser = SubstitutionsParser()
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
 
-    parser.update_metadata()
+bot = Bot(ACCESS_TOKEN)
 
-    date = dates.download_dates()[0]
-    substitution = substitutions.get_substitutions_for_grade(date, grade)
-    
-    return parser.get_full_string(substitution, date)
+substitutions = Substitutions()
+dates = SubstitutionsDates()
+parser = SubstitutionsParser()
+
+parser.update_metadata()
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        token_sent = request.args.get("hub.verify_token")
+        if token_sent == VERIFY_TOKEN:
+            return request.args.get("hub.challenge")
+        else:
+            return "Invalid verification token"
+    else:
+        output = request.get_json()
+        for event in output["entry"]:
+            messaging = event["messaging"]
+            for message in messaging:
+                if message.get("message"):
+                    recipient_id = message["sender"]["id"]
+                    text = message["message"].get("text")
+
+                    date = dates.download_dates()[0]
+                    substitution = substitutions.get_substitutions_for_grade(date, text)
+
+                    bot.send_text_message(recipient_id, parser.get_full_string(substitution, date))
+          
+    return "Message Processed"
