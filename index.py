56cdc3d5d4addbd589380d421d75fa69501357da
bartoszstepien01 <bartoszstepien01@interia.pl>
2022-06-21 22:33:38 +0200

    Database test

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 
+from flask_sqlalchemy import SQLAlchemy
+
 from substitutions.substitutions import Substitutions
 from substitutions.substitutionsdates import SubstitutionsDates
 from substitutions.substitutionsparser import SubstitutionsParser
@@ -9,11 +11,17 @@ from pymessenger.bot import Bot
 import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
 
 bot = Bot(ACCESS_TOKEN)
+
+GRADES = os.getenv("GRADES").split(",")
 
 substitutions = Substitutions()
 dates = SubstitutionsDates()
@@ -21,6 +29,15 @@ parser = SubstitutionsParser()
 
 parser.update_metadata()
 
+class User(db.Model):
+    id = db.Column(db.String(100), primary_key=True)
+    grade = db.Column(db.String(5))
+
+class Date(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+
+db.create_all()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -37,11 +54,29 @@ def index():
             for message in messaging:
                 if message.get("message"):
                     recipient_id = message["sender"]["id"]
-                    text = message["message"].get("text")
+                    text = message["message"].get("text").lower()
 
-                    date = dates.download_dates()[0]
-                    substitution = substitutions.get_substitutions_for_grade(date, text)
+                    user = User.query.filter_by(id=recipient_id).first()
 
-                    bot.send_text_message(recipient_id, parser.get_full_string(substitution, date))
+                    if text in GRADES:
+                        if user:
+                            user.grade = text
+                            bot.send_text_message(recipient_id, "Od teraz będziesz otrzymywać powiadomienia dla klasy {0} 😀. Możesz je wyłączyć wpisując frazę STOP.".format(text))
+                        else:
+                            user = User(id=recipient_id, grade=text)
+                            bot.send_text_message(recipient_id, "Od teraz będziesz otrzymywać powiadomienia za każdym razem, gdy pojawią się nowe zastępstwa dla klasy {0} 😀. Możesz je wyłączyć wpisując frazę STOP.".format(text))
+                            
+                        db.session.add(user)
+                        db.session.commit()
+                    elif text == "stop" and user:
+                        db.session.delete(user)
+                        db.session.commit()
+                        bot.send_text_message(recipient_id, "Od teraz nie będziesz już otrzymywać powiadomień. Aby włączyć je ponownie, wpisz nazwę swojej klasy.")
+                    else:
+                        bot.send_text_message(recipient_id, "Przykro mi, ale nie istnieje klasa o podanej nazwie 😢")
+                elif message.get("postback"):
+                    recipient_id = message["sender"]["id"]
+                    bot.send_text_message(recipient_id, "👋 Witaj!\nAby zacząć otrzymywać powiadomienia, wpisz nazwę swojej klasy")
+
           
     return "Message Processed"
