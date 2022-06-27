02648708882162b72c30eb3b4d55d94a990bdf89
bartoszstepien01 <bartoszstepien01@interia.pl>
2022-06-27 22:55:49 +0200

    Add blueprint to Flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
-from .blueprints.bot import bot
-from .database import db
 from os import getenv
+
+from .blueprints.bot import bot
+from .blueprints.dashboard import dashboard
+
+from .database import db
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
@@ -10,3 +13,4 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
 app.register_blueprint(bot, url_prefix="/bot")
+app.register_blueprint(dashboard, url_prefix="/dashboard")
