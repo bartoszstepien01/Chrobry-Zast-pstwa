6ef834097d9d3c65d81d6afcefc3ddfb6becc32f
bartoszstepien01 <bartoszstepien01@interia.pl>
2022-06-29 01:31:37 +0200

    Stats 3

--- app/database/models/Message.py
+++ app/database/models/Message.py
@@ -1,6 +1,6 @@
 from .. import db
 
-class Setting(db.Model):
+class Message(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(100))
 	value = db.Column(db.Integer)
