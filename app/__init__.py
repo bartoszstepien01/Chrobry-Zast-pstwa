0077e4c1fe170c6a03999ed00fe3cba90df2d551
bartoszstepien01 <bartoszstepien01@interia.pl>
2022-06-29 00:16:08 +0200

    Test 2

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
+db.create_all()
 
 app.register_blueprint(bot, url_prefix="/bot")
 app.register_blueprint(dashboard, url_prefix="/dashboard")
