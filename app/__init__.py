f9f5eef55478bb855382c427c96faee0542b55ab
bartoszstepien01 <bartoszstepien01@interia.pl>
2022-06-29 00:20:04 +0200

    Test 5

--- app/__init__.py
+++ app/__init__.py
@@ -9,10 +9,7 @@ from .database import db
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
 db.init_app(app)
-with app.app_context():
-	db.create_all()
 
 app.register_blueprint(bot, url_prefix="/bot")
 app.register_blueprint(dashboard, url_prefix="/dashboard")
